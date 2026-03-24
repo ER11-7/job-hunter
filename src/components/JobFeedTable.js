75cd196c7966c07c17e60f98c6c68304e1fa7b16
ER11-7 <47293097+ER11-7@users.noreply.github.com>
2026-03-24 15:35:23 +0530

    Create JobFeedTable component

---
+++ src/components/JobFeedTable.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const JobFeedTable = () => {
+    return (
+        <div>
+            <h2>Job Feed</h2>
+            {/* Job feed table code will go here */}
+        </div>
+    );
+};
+
+export default JobFeedTable;
